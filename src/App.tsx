74319cc19b35894dcb83a43e1a618f17b5e76c3e
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-15 20:05:54 -0300

    feat: parte1 del fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,29 @@
+import { useEffect, useState } from "react"
+
 const App = () => {
+  const [todo,setTodo]= useState(null)
+  const [error,setError] = useState("")
 
-
-
-
-
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      try {
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const data = await response.json()
+        setTodo(data)
+      } catch (e) {
+        setError("Error al cargar los datos")
+      }
+    }
+    fetchData()
+  },[])
+  
+  if (error) return <h1 className="text-center text-danger fw-bolder">{error}</h1>
+  if (!todo) return <h1 className="text-center">Cargando datos...</h1>
   return (
     <div className="container mx-auto text-center">
-      <h1>Llamada API con fetch asyn await</h1>
-      <h3 className=" mt-5">El título es:</h3>
-      <h3 className=" mt-5">La tarea está:</h3>
+      <h1>Llamada API con fetch async await</h1>
+      <h3 className=" mt-5">El título es: {todo.title}</h3>
+      <h3 className={`mt-5 ${todo.completed ? "text-success" : "text-danger"}`}>{todo.completed ? "La tarea está completada" : "La tarea está incompleta"}</h3>
     </div>
   )
 }
