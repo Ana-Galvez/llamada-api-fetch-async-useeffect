45a1d0947d81833667c1d8e226a220af39a6bb43
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-15 17:04:13 -0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+const App = () => {
+
+
+
+
+
+  return (
+    <div className="container mx-auto text-center">
+      <h1>Llamada API con fetch asyn await</h1>
+      <h3 className=" mt-5">El título es:</h3>
+      <h3 className=" mt-5">La tarea está:</h3>
+    </div>
+  )
+}
+export default App
