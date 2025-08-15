91c9261305aaf1fb8cf03b3e62b26c01d270cd48
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-15 20:41:43 -0300

    feat: terminado errores

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import { useEffect, useState } from "react"
 
 const App = () => {
-  const [todo,setTodo]= useState(null)
-  const [error,setError] = useState("")
+
+  interface Todo{
+    // userId:number
+    // id:number
+    title:string
+    completed:boolean
+  }
+
+  const [todo,setTodo]= useState<null | Todo>(null)
+  const [error,setError] = useState<null | string>("")
 
   useEffect(()=>{
     const fetchData = async ()=>{
       try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/16")
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/200")
         if (!response.ok) throw new Error("Error 404: la página no existe")
-        const data = await response.json()
+        const data = await response.json() as Todo
         setTodo(data)
       } catch (e:unknown) {
         if (e instanceof Error) return setError(e.message)
@@ -25,7 +33,7 @@ const App = () => {
   return (
     <div className="container mx-auto text-center">
       <h1>Llamada API con fetch async await</h1>
-      <h3 className=" mt-5">El título es: {todo.title.toUpperCase()}</h3>
+      <h3 className="mt-5 text-secondary">El título es: {todo.title.toUpperCase()}</h3>
       <h3 className={`mt-5 ${todo.completed ? "text-success" : "text-danger"}`}>{todo.completed ? "La tarea está completada" : "La tarea está incompleta"}</h3>
     </div>
   )
