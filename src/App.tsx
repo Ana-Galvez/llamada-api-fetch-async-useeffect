2c4208f6f76f6a0c606292f7f7415a3f3787d5b8
Ana-Galvez <anamariagalvez01@gmail.com>
2025-08-15 20:25:49 -0300

    feat: manejo errores

--- src/App.tsx
+++ src/App.tsx
@@ -7,22 +7,25 @@ const App = () => {
   useEffect(()=>{
     const fetchData = async ()=>{
       try {
-        const response = await fetch("https://jsonplaceholder.typicode.com/todos/1")
+        const response = await fetch("https://jsonplaceholder.typicode.com/todos/16")
+        if (!response.ok) throw new Error("Error 404: la página no existe")
         const data = await response.json()
         setTodo(data)
-      } catch (e) {
+      } catch (e:unknown) {
+        if (e instanceof Error) return setError(e.message)
         setError("Error al cargar los datos")
       }
     }
     fetchData()
   },[])
   
-  if (error) return <h1 className="text-center text-danger fw-bolder">{error}</h1>
-  if (!todo) return <h1 className="text-center">Cargando datos...</h1>
+  if (error) return <h1 className="text-center  mt-5 text-danger fw-bolder">{error}</h1>
+  if (!todo) return <h1 className="text-center mt-5">Cargando datos...</h1>
+
   return (
     <div className="container mx-auto text-center">
       <h1>Llamada API con fetch async await</h1>
-      <h3 className=" mt-5">El título es: {todo.title}</h3>
+      <h3 className=" mt-5">El título es: {todo.title.toUpperCase()}</h3>
       <h3 className={`mt-5 ${todo.completed ? "text-success" : "text-danger"}`}>{todo.completed ? "La tarea está completada" : "La tarea está incompleta"}</h3>
     </div>
   )
